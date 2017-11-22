433de22fc150935364a993bcc30d4b956d00cabd
wldyslw <wldyslw@outlook.com>
2017-11-22 21:41:52 +0300

    Initial commit

---
+++ client/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+import './style'
+import 'typeface-roboto'
+
+render(
+    'Test',
+    document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        bundle: './client/app.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './public')
+    },
+    resolve: {
+        extensions: ['.js', '.styl']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                loader: "babel-loader"
+            },
+            {
+                test: /\.styl$|.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader!stylus-loader',
+                    publicPath: '../'
+                })
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.otf$|\.eot$/,
+                loader: 'file-loader?name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.svg$|\.png|\.jpe?g|\.gif|\.ico$/,
+                loader: 'file-loader?name=img/[name].[ext]'
+            },
+            {
+                test: /\.json$/,
+                loader: 'file-loader?name=[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'style/bundle.css',
+            allChunks: true
+        }),
+        // new CleanWebpackPlugin('./public', {
+        //     exclude: [ 'robots.txt' ],
+        //     verbose:  false,
+        //     dry:      false
+        // }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            title: 'АГМ-30Е',
+            // favicon: './client/assets/images/icon.ico',
+            template: './client/templates/template.ejs',
+            appMountId: 'root'
+        })
+    ],
+    devServer: {
+        contentBase: "./public",
+        historyApiFallback: true
+    }
+};
